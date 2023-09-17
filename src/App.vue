1bb5f495e4c18ea33e5cfc2a725803907249d41e
Mateus Santos <mateusclaudinodossantos@hotmail.com>
2023-09-17 15:54:24 -0300

    fullcode

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,45 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+      <b-navbar-brand href="#">ToDo List</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item to="/">Tarefas</b-nav-item>
+          <b-nav-item to="/form">Formulário</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+
+    <transition name="fade" mode="out-in">
+      <router-view />
+    </transition>
+
+    <Footer />
   </div>
 </template>
 
+<script>
+import Footer from "./components/Footer.vue";
+export default {
+  components: {
+    Footer,
+  },
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+.fade-enter-active,
+.fade-leave-active {
+  transition-duration: 0.2s;
+  transition-property: opacity;
+  transition-timing-function: ease;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+.fade-enter,
+.fade-leave {
+  opacity: 0;
 }
 </style>
